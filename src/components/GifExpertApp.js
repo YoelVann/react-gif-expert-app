8d6645086df1c0775f6d1f27b2c8651ef0130358
Joel Hoil <yoelvann@gmail.com>
2022-02-21 22:03:36 -0600

    Firt commit

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Naruto']);
+    
+    // const handleAdd = () =>{
+    //     setCategories([...categories, 'Hunter X Hunter']);
+    // }
+
+    return ( 
+        <>
+        <h2>Gif Expert App</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr></hr>
+        <ol>
+            {
+                categories.map( category => 
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                )    
+            }
+        </ol>
+    
+        </>
+    );
+};
+
+export default GifExpertApp;
